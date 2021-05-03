8cb5b71558e4422e50c8a25da67019efd0d91854
ErnestKufuor <68547652+ErnestKufuor@users.noreply.github.com>
2021-05-03 11:03:37 +0200

    Updated Setup and Added Ner File

--- apollo/setup.py
+++ apollo/setup.py
@@ -5,8 +5,11 @@ import subprocess
 import pkg_resources
 
 #list of required packages to be installed into system
-required = {'numpy','pandas','GoogleNews','goose3','networkx','tqdm','matplotlib','seaborn','py3plex','louvain','python-igraph','leidenalg'} 
+required = {'numpy','pandas','GoogleNews','goose3','networkx','tqdm','matplotlib','seaborn','py3plex','louvain','python-igraph','leidenalg','torch','flair','nltk'} 
 
 if required:
     # implement pip as a subprocess:
     subprocess.check_call([sys.executable, '-m', 'pip', 'install',*required])
+    
+from flair.models import SequenceTagger
+tagger = SequenceTagger.load('ner')
